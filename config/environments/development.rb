ad714b19d606e0b8933fb56ac184180008c352df
Your Name <01namangupta@gmail.com>
2017-10-15 19:10:16 +0530

    minor error is removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  cconfig.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
